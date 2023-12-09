83c531338f9396ab1bce2173715269fa54982a24
Samuel Odumu <themainsamuel@gmail.com>
2023-12-09 09:56:53 +0100

    insertion in a linked list

---
+++ arrays_and_linked_lists/3-linked-list-insertion.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+	int data;
+	struct node *next;
+} node;
+
+node *head = NULL;
+node *current = NULL;
+
+void printlist()
+{
+	node *p = head;
+	printf("\n[");
+
+	while (p != NULL)
+	{
+		printf(" %d ", p->data);
+		p = p->next;
+	}
+	printf("]\n");
+}
+
+void insertatbegin(int data)
+{
+	node *nl;
+	nl = malloc(sizeof(node));
+	nl->data = data;
+	nl->next = head;
+
+	head = nl;
+}
+
+void insertatend(int data)
+{
+	node *nl;
+
+	nl = malloc(sizeof(node));
+	nl->data = data;
+	node *lastnode = head;
+
+	while (lastnode->next != NULL)
+	{
+		lastnode = lastnode->next;
+	}
+	lastnode->next = nl;
+}
+
+
+int main(void)
+{
+	insertatbegin(12);
+	insertatend(14);
+	insertatbegin(10);
+	insertatbegin(8);
+	insertatbegin(6);
+	insertatbegin(4);
+
+	printlist();
+	return (0);
+}
